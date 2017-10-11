bf238dec74389d2bc2821d30ad1658cd2c85f3cd
nic <nictamura@gmail.com>
2017-10-11 16:43:18 -0700

    randomizer team

--- js/teams.js
+++ js/teams.js
@@ -1,44 +1,46 @@
-//   nic, bob, jes, jul, jon, yak, cat, kels, bra, howl, mark
 $(function(){
-  let inputNames = $('#inputNames').val();
-  let inputNums = $('#inputNums').val();
+
+  $( "#teamButton" ).click(function() {
+    let inputNames = $('#inputNames').val();
+    let inputNums = $('#inputNums').val();
+        inputNames = inputNames.split(', ')
+        inputNames = shuffle(inputNames);
+        teamRandomizer(inputNames, inputNums)
+        // renderHtml(teamRandomizer)
+
+    function shuffle(arr) {
+      let currentIndex = arr.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+      }
+      return arr;
+    }
+
+    function teamRandomizer(names, teamCount) {
+      let team = {};
+      let teamCounter = 0;
+
+      for (let i=0; i<names.length; ++i) {
+        if (!team["Team" + teamCounter]) {
+            team["Team" + teamCounter] = [];
+        }
+        team["Team" + teamCounter].push(names[i]);
+        if (++teamCounter == teamCount) {
+            teamCounter = 0;
+        }
+      }
+      // for(t of team){
+      //   $('#teamOutput').html(`<p>${Object[team]}</p>`);
+      // }
+      console.log(team);
+      // console.log(Object(team));
+    }
 
 
 
+  });
 });
-
-// let inputNames = prompt('team names')
-    inputNames = inputNames.split(', ')
-    inputNames = shuffle(inputNames);
-// let inputNums = prompt('number of teams')
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-
-function teamRandomizer(names, teamCount) {
-  var team = {};
-  var teamCounter = 0;
-
-  for (var i = 0; i < names.length; ++i) {
-    if (!team["Team" + teamCounter]) {
-        team["Team" + teamCounter] = [];
-    }
-    team["Team" + teamCounter].push(names[i]);
-    if (++teamCounter == teamCount) {
-        teamCounter = 0;
-    }
-  }
-  return team;
-}
-
-teamRandomizer(inputNames, inputNums)
