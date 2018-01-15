f5dcebc8f4a7c492ed58c32c89e922ed8ae4aab0
nTamura <nictamura@gmail.com>
2018-01-15 09:47:59 -0800

    change team number

--- js/teams.js
+++ js/teams.js
@@ -25,8 +25,8 @@
       let teamCounter = 0;
 
       for (let i=0; i<names.length; ++i) {
-        if (!team["Team" + teamCounter + 1]) {
-          team["Team" + teamCounter] = [];
+        if (!team["Team " + teamCounter + 1]) {
+          team["Team " + teamCounter+1] = [];
         }
         team["Team" + teamCounter].push(names[i]);
         if (++teamCounter == teamCount) {
